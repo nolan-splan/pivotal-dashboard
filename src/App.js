66c10932c35e8fb80aab5a77afe848241e3aa556
Nolan Splan <nolan.splan@ccisystems.com>
2021-12-07 13:10:23 -0600

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button, Container, Divider, Paper, Stack, TextField, Typography } from '@mui/material';
+import React from 'react'
+import { fetchProject, fetchIterations } from './pivotal_api';
 
 function App() {
+  const reducer = (acc, currentVal) => acc + currentVal
+  const uniq = (val, index, self) => self.indexOf(val) === index
+
+  const getNameFromId = (id) => {
+    if (id === 3138117) {
+      return "Richy"
+    } else if (id === 1325118) {
+      return "Dom"
+    } else if (id === 2896513) {
+      return "Nolan"
+    } else if (id === 1491212) {
+      return "Jason"
+    }
+  }
+
+  const sumIterationPoints = (stories) => {
+    const filtered = stories.filter(story => !["bug", "chore"].includes(story.story_type))
+    const points = filtered.map(story => story.estimate)
+    const sum = points.reduce(reducer)
+    return sum
+  }
+
+  const handleProjectIdChanged = (event) => {
+    setProjectId(event.target.value)
+  }
+
+  const handleFetchProjectClicked = (event) => {
+    fetchProject(projectId, setProject)
+  }
+
+  const handleFetchProjectIterationsClicked = (event) => {
+    fetchIterations(projectId, setIterations)
+  }
+
+  const [projectId, setProjectId] = React.useState("866453")
+  const [project, setProject] = React.useState({})
+  const [iterations, setIterations] = React.useState([])
+
+  console.log('Project ID: ', projectId)
+  console.log('Project: ', project)
+  console.log('Iterations: ', iterations)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container fluid="lg" style={{ marginTop: '1rem' }}>
+      <Stack direction="row" spacing={2}>
+        <TextField id="project-id" label="Project ID" variant="outlined" value={projectId} onChange={handleProjectIdChanged}/>
+        <Button id="fetch-project-button" color="primary" size="large" variant="outlined" onClick={handleFetchProjectClicked}>Fetch Project</Button>
+        <Button id="fetch-project-iterations-button" color="secondary" size="large" variant="outlined" onClick={handleFetchProjectIterationsClicked}>Fetch Project Iterations</Button>
+      </Stack>
+      { iterations.length > 0 && (
+        <Stack direction="row" spacing={2}>
+          { iterations.map(iteration => {
+            const totalIterationPoints = sumIterationPoints(iteration.stories)
+            return (
+              <Paper key={iteration.number} style={{ padding: '1rem', width: '100%' }}>
+                <Typography variant="h5">Iteration #{iteration.number}</Typography>
+                <Divider />
+                <Typography variant="body2">Story Count: {iteration.stories.length}</Typography>
+                <Typography variant="body2">Total Points: {totalIterationPoints}</Typography>
+                { iteration.stories.map(story => story.owned_by_id).filter(uniq).map(userId => {
+                  let storiesForUser = iteration.stories.filter(story => story.owned_by_id === userId)
+                  let totalPointsForUser = storiesForUser.filter(story => !["bug", "chore"].includes(story.story_type)).map(story => story.estimate).reduce(reducer)
+                  return (
+                    <Typography key={userId} variant="body2">{getNameFromId(userId)}: {totalPointsForUser} ({Math.floor((totalPointsForUser / totalIterationPoints) * 100)}%)</Typography>
+                  )
+                }) }
+              </Paper>
+            )
+          })}
+        </Stack>
+      )}
+    </Container>
   );
 }
 
---
+++ src/pivotal_api.js
@@ -0,0 +1,38 @@
+const headers = new Headers({ 'X-TrackerToken': process.env.REACT_APP_PIVOTAL_API_TOKEN })
+export function fetchMe() {
+  var options = { method: 'GET', headers: headers }
+  performRequest("https://www.pivotaltracker.com/services/v5/me", options)
+}
+
+export function fetchMyPeople() {
+  var options = { method: 'GET', headers: headers }
+  performRequest("https://www.pivotaltracker.com/services/v5/my/people?project_id=866453", options)
+}
+
+export function fetchMyNotifications() {
+  var options = { method: 'GET', headers: headers }
+  performRequest("https://www.pivotaltracker.com/services/v5/my/notifications?envelope=true", options)
+}
+
+export function fetchMyProjects() {
+  var options = { method: 'GET', headers: headers }
+  performRequest("https://www.pivotaltracker.com/services/v5/projects", options)
+}
+
+export function fetchProject(projectId, callback) {
+  var options = { method: 'GET', headers: headers }
+  performRequest(`https://www.pivotaltracker.com/services/v5/projects/${projectId}`, options)
+    .then(res => res.json())
+    .then(json => callback(json))
+}
+
+export async function fetchIterations(projectId, callback) {
+  var options = { method: 'GET', headers: headers }
+  performRequest(`https://www.pivotaltracker.com/services/v5/projects/${projectId}/iterations?scope=done&offset=-4&limit=4`, options)
+    .then(res => res.json())
+    .then(json => callback(json))
+}
+
+function performRequest(url, options) {
+  return fetch(url, options)
+}
